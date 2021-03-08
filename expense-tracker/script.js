da56852890484ef79d47af9110082889a02db120
wen <wentungwen@gmail.com>
2021-03-08 18:07:03 +0800

    tracker

---
+++ expense-tracker/script.js
@@ -0,0 +1,8 @@
+const balance=document.getElementById('balance')
+const moneyPlus=document.getElementById('money-plus')
+const moneyMinus=document.getElementById('money-minus')
+const list=document.getElementById('list')
+const form=document.getElementById('form')
+const text=document.getElementById('text')
+const amount=document.getElementById('amount')
+const addBtn=document.querySelector('add')
